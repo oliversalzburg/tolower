ef54d4993a67c7b99b7ef1a38e195efc3a33892a
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-02-14 23:05:24 +0100

    Initial commit

---
+++ tolower/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tolower {
+  /// <summary>
+  /// Turns intput into lower case
+  /// </summary>
+  static class Program {
+    /// <summary>
+    /// Main entry point
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main( string[] args ) {
+      string joinedInput = String.Join( " ", args );
+      Console.WriteLine( joinedInput.ToLowerInvariant() );
+    }
+  }
+}
